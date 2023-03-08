67baeab7259d7d2418338b820659937853348b96
Radek Stibora <rstibora@gmail.com>
2023-03-08 14:54:55 +0100

    Setup the project

---
+++ db-service/initialize_seznam.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+psql -v ON_ERROR_STOP=1 --username=postgres<<-EOSQL
+     CREATE DATABASE seznam;
+     CREATE USER seznam WITH PASSWORD '${SEZNAM_DB_PASSWORD}';
+     GRANT ALL PRIVILEGES ON DATABASE seznam TO seznam;
+EOSQL
