4884e40a81f6d63d8030f51ac148d3c2657e0eac
Radek Stibora <rstibora@gmail.com>
2023-03-10 13:06:31 +0100

    Add details page

--- backend-service/seznam/seznam_django/videos/views.py
+++ backend-service/seznam/seznam_django/videos/views.py
@@ -1,13 +1,13 @@
 from typing import Any, Dict, Sequence
 
+from django.db.models import Q
 from django.views import generic
 
-from .models import Metadata
+from .models import Drm, Feature, Metadata
 from .tasks import fetch_and_store_metadata
 
 
 class IndexView(generic.ListView):
-    model = Metadata
     template_name = "videos/index.html"
 
     def get_ordering(self) -> Sequence[str]:
@@ -17,6 +17,28 @@ class IndexView(generic.ListView):
         order_by = self.request.GET.get("order_by", "name")
         return [f"{order_prefix}{order_by}"]
 
+    def get_queryset(self):
+        is_featured = self.request.GET.get("is_featured", None)
+        drms = [drm.removeprefix("filter_drm_") for drm in self.request.GET.keys() if drm.startswith("filter_drm_")]
+        features = [feature.removeprefix("filter_feature_") for feature in self.request.GET.keys() if feature.startswith("filter_feature_")]
+
+        is_featured_q = Q(is_featured=True) if is_featured is not None else ~Q(pk__in=[])
+        drms_q = Q(drms__name__in=drms) if drms else ~Q(pk__in=[])
+        features_q = Q(features__name__in=features) if features else ~Q(pk__in=[])
+
+        return Metadata.objects.all().filter(is_featured_q & drms_q & features_q)
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context_data = super().get_context_data(**kwargs)
+        context_data["all_drms"] = [data[0] for data in Drm.objects.values_list("name")]
+        context_data["all_features"] = [data[0] for data in Feature.objects.values_list("name")]
+        return context_data
+
     def get(self, request, *args, **kwargs):
         fetch_and_store_metadata()
         return super().get(request, *args, **kwargs)
+
+
+class DetailView(generic.DetailView):
+    model = Metadata
+    template_name = "videos/detail.html"
